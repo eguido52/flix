9a4cb71ca7a270de9ced4c5754577df6260e75db
eguido52 <eguido@csumb.edu>
2019-01-28 15:27:55 -0800

    Initial Commit

---
+++ cst495-flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cst495-flix
+//
+//  Created by Emmanuel Guido on 1/28/19.
+//  Copyright © 2019 eguido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
