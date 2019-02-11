a33b1c74682c2593111f0cc85d9d63f0eaf75e42
eguido52 <eguido@csumb.edu>
2019-02-11 13:23:17 -0800

    Completed Part 2

---
+++ cst495-flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  cst495-flix
+//
+//  Created by Emmanuel Guido on 2/5/19.
+//  Copyright © 2019 eguido. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
